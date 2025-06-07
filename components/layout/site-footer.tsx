9f3ee6529004dd59812a4bb28d710bc4d8e94981
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-08 01:10:44 +0530

    update: synced local and remote environments

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -4,7 +4,7 @@ import { Shield } from "lucide-react"
 export function SiteFooter() {
   return (
     <footer className="border-t py-6 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
+      <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row"> 
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Link href="/" className="flex items-center gap-2">
             <Shield className="h-5 w-5 text-primary" />
