89d0cbced321f4b273888f6aaae3c1814e258a1e
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-08 01:09:43 +0530

    update: adjusted script behavior and dependencies

--- components/home/safety-tip.tsx
+++ components/home/safety-tip.tsx
@@ -10,7 +10,7 @@ interface SafetyTipProps {
 }
 
 export function SafetyTip({ tip, index }: SafetyTipProps) {
-  return (
+  return ( 
     <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
