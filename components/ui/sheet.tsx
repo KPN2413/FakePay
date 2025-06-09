3f369a9898250249a0de4abdcb507a36a0c04a6b
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-09 23:37:13 +0530

    chore: updated internal comments and TODOs

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -8,7 +8,7 @@ import { X } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 const Sheet = SheetPrimitive.Root;
-
+ 
 const SheetTrigger = SheetPrimitive.Trigger;
 
 const SheetClose = SheetPrimitive.Close;
