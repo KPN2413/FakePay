a1dfb32530952ef4aae2e282671df6ac69d45edc
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-10 22:09:05 +0530

    chore: realigned folder structure and paths

--- components/ui/toggle.tsx
+++ components/ui/toggle.tsx
@@ -15,7 +15,7 @@ const toggleVariants = cva(
         outline:
           'border border-input bg-transparent hover:bg-accent hover:text-accent-foreground',
       },
-      size: {
+      size: { 
         default: 'h-10 px-3',
         sm: 'h-9 px-2.5',
         lg: 'h-11 px-5',
