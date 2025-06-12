9d5b7fc0f3abab433a8538ccbed7239f6ef4cc8c
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-12 18:05:45 +0530

    cleanup: regular cleanup cycle

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -10,7 +10,7 @@ import { buttonVariants } from '@/components/ui/button';
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
-  className,
+  className, 
   classNames,
   showOutsideDays = true,
   ...props
