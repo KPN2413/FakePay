592003f6bb19b688ccc7870b926ae70704f93c86
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-13 00:55:24 +0530

    update: aligned config files and structure

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -13,7 +13,7 @@ const Command = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof CommandPrimitive>
 >(({ className, ...props }, ref) => (
   <CommandPrimitive
-    ref={ref}
+    ref={ref} 
     className={cn(
       'flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground',
       className
