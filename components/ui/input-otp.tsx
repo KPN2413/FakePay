f0577cc6dfd973c71a32006e6101f78a50206081
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-15 23:39:45 +0530

    Update input-otp.tsx

--- components/ui/input-otp.tsx
+++ components/ui/input-otp.tsx
@@ -9,7 +9,7 @@ import { cn } from '@/lib/utils';
 
 const InputOTP = React.forwardRef<
   React.ElementRef<typeof OTPInput>,
-  React.ComponentPropsWithoutRef<typeof OTPInput>
+  React.ComponentPropsWithoutRef<typeof OTPInput> 
 >(({ className, containerClassName, ...props }, ref) => (
   <OTPInput
     ref={ref}
