887aba6d66774cb6201bf3d41fc45c3a2a9f5bd7
K Prakhar Narayan <prakharnarayan.k@gmail.com>
2025-06-17 18:04:25 +0530

    Update tabs.tsx

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -10,7 +10,9 @@ const Tabs = TabsPrimitive.Root
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
   React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
+>(({ className, ...props }, ref) => ( 
+
+ 
   <TabsPrimitive.List
     ref={ref}
     className={cn(
